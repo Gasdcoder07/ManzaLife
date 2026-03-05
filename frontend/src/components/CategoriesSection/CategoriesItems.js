7c6084310adcfe78f94fa5c7f225a222375196c5
IsahitMartinez <mmartinez134@ucol.mx>
2026-03-05 09:00:17 -0600

    Cambio en imagenes de categorias

--- frontend/src/components/CategoriesSection/CategoriesItems.js
+++ frontend/src/components/CategoriesSection/CategoriesItems.js
@@ -3,7 +3,11 @@ import { IoMdRestaurant } from "react-icons/io";
 import { FaHotel, FaUmbrellaBeach, FaPlaceOfWorship } from "react-icons/fa6";
 import { LuSunMoon } from "react-icons/lu";
 import Image1 from "../../../imgs/HomeResources/peñablanca.jpg"
+import image3 from "../../../imgs/HomeResources/Patasalada.png"
 import Image2 from "../../../imgs/HomeResources/Sunset.jpeg"
+import image4 from "../../../imgs/HomeResources/feria.png"
+import image5 from "../../../imgs/HomeResources/Pezvela.png"
+import image6 from "../../../imgs/HomeResources/Barcelo.jpg"
 
 export const CategoriesItems = [
     {
@@ -15,25 +19,25 @@ export const CategoriesItems = [
     {
         id: 2,
         icon: IoMdRestaurant,
-        image: Image1,
+        image: image3,
         title: "Restaurantes"
     },
     {
         id: 3,
         icon: FaUmbrellaBeach,
-        image: Image1,
+        image: image4,
         title: "Actividades"
     },
     {
         id: 4,
         icon: FaHotel,
-        image: Image1,
+        image: image6,
         title: "Hospedaje"
     },
     {
         id: 5,
         icon: FaPlaceOfWorship,
-        image: Image1,
+        image: image5,
         title: "Lugares turísticos"
     },
     {
