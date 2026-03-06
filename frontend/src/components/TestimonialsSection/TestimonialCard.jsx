60e75db325cdadf478f4718485fd42968a9e56e1
IsahitMartinez <mmartinez134@ucol.mx>
2026-03-06 11:09:04 -0600

    Creacion de testimonials cards

---
+++ frontend/src/components/TestimonialsSection/TestimonialCard.jsx
@@ -0,0 +1,34 @@
+import { FaStar } from "react-icons/fa";
+const TestimonialCard = ({ img, nombre, ocupacion, testimonio }) => {
+  return (  
+        <div className="bg-linear-to-b from-orange-500 via-orange-600 to-orange-700 max-w-xs px-8 py-6 rounded-md flex flex-col items-center gap-6">
+
+            <div className="flex justify-center">
+                <img
+                    className="rounded-full size-20 object-cover border border-neutral-900"
+                    src={img}
+                    alt="" />
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <h3 className="font-bold text-white text-2xl text-center tracking-wider">{nombre}</h3>
+                <p className="text-neutral-700 text-center text-sm">{ocupacion}</p>
+            </div>
+
+            <p className="text-neutral-900 tracking-wider">
+                {testimonio}
+            </p>
+            
+            <div className="flex justify-center items-center gap-2">
+                {
+                    Array.from({ length: 5 }).map((_, i) => (
+                        <FaStar className="text-yellow-400 text-2xl"/>
+                    ))
+                }
+            </div>
+
+        </div>
+  )
+}
+
+export default TestimonialCard
