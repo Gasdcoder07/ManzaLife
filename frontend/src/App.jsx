7e6e8ca91ffa4a05c031b13dc8f6d51764fe27e5
IsahitMartinez <mmartinez134@ucol.mx>
2026-03-15 17:50:13 -0600

    diseño a pagina de publicar posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Home, Login, Register, BlogHome, Pruebas} from "./pages/index.js"
+import { Home, Login, Register, BlogHome, Pruebas, Posts} from "./pages/index.js"
 import BlogLayout from "./layouts/BlogLayout.jsx";
 import { Routes, Route } from 'react-router-dom'
 
@@ -20,6 +20,8 @@ function App() {
                   </Route>
 
                   <Route path="/pruebas" element={<Pruebas />} />
+                  <Route path="/posts" element={<Posts />} />
+
               </Routes>
           </main>
       </div>
