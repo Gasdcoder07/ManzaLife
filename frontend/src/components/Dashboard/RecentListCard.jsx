3671c73d359f7c4a1161b06815eb55e3997bad73
Valent-x7 <valentinvacacipres@gmail.com>
2026-04-30 10:07:08 -0600

    RecentListCard para publicaciones y usuarios.

--- frontend/src/components/Dashboard/RecentListCard.jsx
+++ frontend/src/components/Dashboard/RecentListCard.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
 
-const RecentListCard = ({ Title, Posts = null, Comments = null }) => {
+const RecentListCard = ({ Classname, Title, Data, Type }) => {
   return (
-    <div className="h-64 bg-[#fffbf8] dark:bg-[#0d0d0f] border border-neutral-300 dark:border-neutral-800 rounded-xl shadow-xl px-2 py-3 flex flex-col gap-2.5">
+    <div className={`${Classname} h-64 bg-[#fffbf8] dark:bg-[#0d0d0f] border border-neutral-300 dark:border-neutral-800 rounded-xl shadow-xl px-2 py-3 flex flex-col gap-2.5`}>
         <p className="text-xl px-3">{Title}</p>
 
         {/* Tabla */}
@@ -10,33 +10,39 @@ const RecentListCard = ({ Title, Posts = null, Comments = null }) => {
             <table className="w-full table-fixed text-left rounded overflow-hidden">
                 <thead className="bg-zinc-100 dark:bg-zinc-950 text-neutral-600 dark:text-neutral-200">
                     <tr>
-                        <th className="px-3 py-1.5">Titulo</th>
-                        <th className="px-3 py-1.5">Autor</th>
-                        <th className="px-3 py-1.5">Estado</th>
+                        <th className="px-3 py-1.5">{Type === "Post" ? 'Titulo' : 'Usuario'}</th>
+                        <th className="px-3 py-1.5">{Type === "Post" ? 'Autor' : 'Email'}</th>
+                        {
+                            Type === "Post" && (
+                                <th className="px-3 py-1.5">Estado</th>
+                            )
+                        }
                         <th className="px-3 py-1.5">Acciones</th>
                     </tr>
                 </thead>
 
                 <tbody className="divide-y divide-neutral-200 dark:divide-neutral-800">
                     {
-                        Posts && (
-                            Posts.map((post, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td className="px-3 py-1.5 truncate">{post.title}</td>
-                                        <td className="px-3 py-1.5 truncate">{post.author_name}</td>
-                                        <td className="px-3 py-1.5 text-green-500 dark:text-green-700">{post.status}</td>
-                                        <td className="px-3 py-1.5">
-                                            <Link
-                                                to={`/blog/${post.slug}`}
-                                                className='flex justify-center items-center rounded-full w-2/3 mx-auto bg-blue-100 dark:bg-blue-900 text-blue-500 hover:text-blue-400'>
-                                                <span>Vista</span>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        )
+                        Data?.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td className="px-3 py-1.5 truncate">{Type === "Post" ? item.title : item.username}</td>
+                                    <td className="px-3 py-1.5 truncate">{Type === "Post" ? item.author_name : item.email}</td>
+                                    {
+                                        Type === "Post" && (
+                                            <td className="px-3 py-1.5 text-green-500 dark:text-green-700">{Type === "Post" ? item.status : "..."}</td>
+                                        )
+                                    }
+                                    <td className="px-3 py-1.5">
+                                        <Link
+                                            to={Type === "Post" ? `/blog/${item.slug}` : `/blog/profile/${item.username}`}
+                                            className='flex justify-center items-center rounded-full w-full mx-auto bg-blue-100 hover:bg-blue-200 text-blue-400 dark:bg-blue-900 dark:hover:bg-blue-950 dark:text-blue-500 transition-all duration-200 ease-in-out'>
+                                            <span>Vista</span>
+                                        </Link>
+                                    </td>
+                                </tr>
+                            )
+                        })
                     }
                 </tbody>
             </table>
