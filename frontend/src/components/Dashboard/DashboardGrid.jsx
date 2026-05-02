3871d430901efe6e8a619035a9ff343d5c76cde6
Valent-x7 <valentinvacacipres@gmail.com>
2026-05-02 13:10:36 -0600

    Refactor Dashboard components to support multilingual descriptions and replace RecentListCard with RecentPostsCard and RecentUsersCard.

--- frontend/src/components/Dashboard/DashboardGrid.jsx
+++ frontend/src/components/Dashboard/DashboardGrid.jsx
@@ -1,11 +1,12 @@
+import StatsCard from "./StatsCard";
+import RecentPostsCard from "./RecentPostsCard";
+import RecentUsersCard from "./RecentUsersCard";
 import { MdArticle } from "react-icons/md";
 import { BiSolidCategory } from "react-icons/bi";
 import { FaUsers } from "react-icons/fa6";
 import { IoMailUnread } from "react-icons/io5";
-import StatsCard from "./StatsCard";
-import RecentListCard from "./RecentListCard";
 
-const DashboardGrid = ({ DashboardStats, LatestPosts, LatestUsers, LoadingStats }) => {
+const DashboardGrid = ({ isEnglish, DashboardStats, LatestPosts, LatestUsers, LoadingStats }) => {
   return (
     <div className="h-full flex flex-col gap-4">
         <div className="flex flex-col py-4 gap-4">
@@ -13,42 +14,38 @@ const DashboardGrid = ({ DashboardStats, LatestPosts, LatestUsers, LoadingStats 
             <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 w-full">
                 <StatsCard
                     Icon={MdArticle}
-                    Description={"Publicaciones totales"}
+                    Description={isEnglish ? 'Total posts' : 'Publicaciones totales'}
                     Number={DashboardStats.posts}
                     Loading={LoadingStats} />
 
                 <StatsCard
                     Icon={BiSolidCategory}
-                    Description={"Categorias totales"}
+                    Description={isEnglish ? "Total categories" : "Categorias totales"}
                     Number={DashboardStats.categories}
                     Loading={LoadingStats} />
 
                 <StatsCard
                     Icon={FaUsers}
-                    Description={"Usuarios registrados"}
+                    Description={isEnglish ? "Registered users" : "Usuarios registrados"}
                     Number={DashboardStats.users}
                     Loading={LoadingStats} />
 
                 <StatsCard
                     Icon={IoMailUnread}
-                    Description={"Solicitudes pendientes"}
+                    Description={isEnglish ? "Pending requests" : "Solicitudes pendientes"}
                     Number={6}
                     Loading={LoadingStats} />
             </div>
 
-            {/* RecentListCard */}
+            {/* RecentCards */}
             <div className="grid grid-cols-1 xl:grid-cols-5 gap-4 w-full">
-                <RecentListCard
+                <RecentPostsCard
                     Classname={"xl:col-span-3"}
-                    Title={"Publicaciones recientes"}
-                    Data={LatestPosts}
-                    Type={"Post"}/>
+                    Posts={LatestPosts}/>
 
-                <RecentListCard
+                <RecentUsersCard
                     Classname={"xl:col-span-2"}
-                    Title={"Usuarios recientes"}
-                    Data={LatestUsers}
-                    Type={"User"}/>
+                    Users={LatestUsers}/>
             </div>
         </div>
     </div>
---
+++ frontend/src/components/Dashboard/RecentPostsCard.jsx
@@ -0,0 +1,76 @@
+import { useLanguage } from "../../context/LanguageContext";
+import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { MdDelete, MdModeEdit } from "react-icons/md";
+
+const RecentPostsCard = ({ Classname, Posts }) => {
+    const { idioma } = useLanguage();
+    const isEnglish = idioma === "en";
+
+    return (
+        <div className={`${Classname} h-64 bg-[#fffbf8] dark:bg-[#0d0d0f] border border-neutral-300 dark:border-neutral-800 rounded-xl shadow-xl px-2 py-3 flex flex-col gap-2.5`}>
+            <p className="text-xl px-3 font-semibold">
+                {isEnglish ? 'Recent posts' : 'Publicaciones recientes'}
+            </p>
+
+            {/* Tabla */}
+            <div className="flex-1 overflow-auto custom-scrollbar">
+                <table className="w-full table-fixed text-left rounded overflow-hidden">
+                    <thead className="bg-zinc-100 dark:bg-zinc-950 text-neutral-600 dark:text-neutral-200">
+                        <tr>
+                            <th className="px-3 py-1.5">
+                                {isEnglish ? 'Title' : 'Titulo'}
+                            </th>
+                            <th className="px-3 py-1.5">
+                                {isEnglish ? 'Author' : 'Autor'}
+                            </th>
+                            <th className="px-3 py-1.5">
+                                {isEnglish ? 'Status' : 'Estado'}
+                            </th>
+                            <th className="px-3 py-1.5 shrink-0">
+                                {isEnglish ? 'Actions' : 'Acciones'}
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="divide-y divide-neutral-200 dark:divide-neutral-800">
+                        {
+                            Posts?.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className="px-3 py-1.5 truncate">{item.title}</td>
+                                        <td className="px-3 py-1.5 truncate">{item.author_name}</td>
+                                        <td className="px-3 py-1.5 text-green-500 dark:text-green-700 truncate">{item.status}</td>
+                                        <td className="shrink-0 px-3 py-1.5 flex gap-4">
+                                            <Link
+                                                to={`/blog/${item.slug}`}
+                                                className='flex justify-center items-center hover:text-blue-500 dark:hover:text-blue-800 transition-colors ease-in-out duration-200'>
+                                                <FaEye/>
+                                            </Link>
+
+                                            <div className="flex justify-center items-center gap-4">
+                                                <Link
+                                                    className="flex justify-center"
+                                                    to={`/edit-post/${item.slug}`}>
+                                                    <button className="cursor-pointer transition-all duration-200 ease-in-out hover:text-yellow-600">
+                                                        <MdModeEdit/>
+                                                    </button>
+                                                </Link>
+                                                <button
+                                                    className="cursor-pointer transition-all duration-200 ease-in-out hover:text-red-600">
+                                                    <MdDelete/>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default RecentPostsCard;
