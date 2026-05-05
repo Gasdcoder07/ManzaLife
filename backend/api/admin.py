2a06899fcf16d625069413c86c808e8d4d1e1878
Valent-x7 <valentinvacacipres@gmail.com>
2026-05-05 11:23:28 -0600

    Register SystemRequest model in the admin interface

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment, Like, Category, UserProfile, Review
+from .models import Post, Comment, Like, Category, UserProfile, Review, SystemRequest
 
 admin.site.register(Post)
 admin.site.register(Comment)
@@ -7,6 +7,7 @@ admin.site.register(Like)
 admin.site.register(Category)
 admin.site.register(UserProfile)
 admin.site.register(Review)
+admin.site.register(SystemRequest)
 
 
 
