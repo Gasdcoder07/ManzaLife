89d49544203ad63a2f4ef785b7151ddff1472129
Valent-x7 <valentinvacacipres@gmail.com>
2026-05-06 19:24:07 -0600

    feat: enhance RequestModal with loading feedback and improved toast messages

--- frontend/src/components/Modals/RequestModal.jsx
+++ frontend/src/components/Modals/RequestModal.jsx
@@ -61,22 +61,36 @@ const RequestModal = ({ setShowModal, setRequests }) => {
 
         setLoading(true);
 
+        const toastId = toast.loading(
+            isEnglish
+                ? 'Sending request...'
+                : 'Enviando solicitud...'
+        )
+
         try {
             const response = await postRequest(formData);
 
             toast.success(
                 isEnglish
-                    ? 'Request succesfull'
-                    : 'Exito mi pa'
-            )
+                    ? 'Request submitted successfully.'
+                    : 'Solicitud enviada correctamente.',
+                {
+                    id: toastId,
+                }
+            );
+
             setShowModal(false);
         }
         catch (e) {
             toast.error(
                 isEnglish
-                    ? 'Error'
-                    : 'Error'
-            )
+                    ? 'Failed to send request.'
+                    : 'Fallo al enviar la solicitud.',
+                {
+                    id: toastId,
+                }
+            );
+
             console.error("Error al crear solicitud: ", e);
         }
         finally {
@@ -148,7 +162,7 @@ const RequestModal = ({ setShowModal, setRequests }) => {
                 <button
                     disabled={loading}
                     onClick={handleClick}
-                    className={`${loading && 'bg-zinc-700 text-zinc-500'}text-white bg-orange-600 hover:-translate-y-1 transition-all duration-200 ease-in-out cursor-pointer px-4 py-2 rounded`}>
+                    className={`${loading ? 'bg-zinc-700 text-zinc-500 cursor-not-allowed' : 'text-white bg-orange-600 hover:-translate-y-1 cursor-pointer'} transition-all duration-200 ease-in-out px-4 py-2 rounded`}>
                     {isEnglish ? "Send" : "Enviar"}
                 </button>
             </div>
