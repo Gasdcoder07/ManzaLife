5a1875432b61352ea237239b20ab9cf51278c7f0
Gasdcoder07 <gserna@ucol.mx>
2026-05-17 22:28:53 -0600

    Fix: Change ban/unban requests from POST to PATCH

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -13,11 +13,11 @@ export const getUserByUsername = async (username) => {
 }
 
 export const banUser = async (userId, reason) => {
-    const res = await api.post(`usuarios/${userId}/ban/`, { reason });
+    const res = await api.patch(`usuarios/${userId}/ban/`, { reason });
     return res.data;
 }
 
 export const unbanUser = async (userId) => {
-    const res = await api.post(`usuarios/${userId}/unban/`);
+    const res = await api.patch(`usuarios/${userId}/unban/`);
     return res.data;
 }
