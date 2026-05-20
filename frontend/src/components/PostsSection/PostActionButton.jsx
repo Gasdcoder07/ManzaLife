6d4c671aa2660c6e16d2b119ed4d29d73de23959
Valent-x7 <valentinvacacipres@gmail.com>
2026-05-20 14:53:01 -0600

    Feat: Add isEnglish prop to various components for localization support

--- frontend/src/components/PostsSection/PostActionButton.jsx
+++ frontend/src/components/PostsSection/PostActionButton.jsx
@@ -1,9 +1,9 @@
 import { FaArrowRight } from "react-icons/fa6";
 
-const PostActionButton = () => {
+const PostActionButton = ({ isEnglish }) => {
   return (
     <div className="flex justify-center items-center gap-2 bg-black/5 dark:bg-neutral-800 px-3 py-2 rounded-xl text-sm hover:text-orange-600 cursor-pointer transition-all duration-200 ease-in-out">
-        <span>Seguir leyendo</span>
+        <span>{isEnglish ? "Continue reading" : "Seguir leyendo"}</span>
         <FaArrowRight/>
     </div>
   );
